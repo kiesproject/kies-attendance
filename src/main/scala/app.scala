d5a459e16b44bc963a467e03ea6abb76d0969a9e
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2018-04-19 00:14:39 +0900

    [wip] use db

--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -9,9 +9,19 @@ object Hello extends WebApp {
   // x8pzmgnILG337ylj
   get("/say-hello") {
     s"Hello, ${params.getOrElse("message", "Anonymous")}!\n"
-  }
 
-  DriverManager.getConnection("", "", "")
+//    val connection = DriverManager.getConnection("jdbc:google:mysql://myinstance/users", "root", "x8pzmgnILG337ylj")
+//    val statement = connection.createStatement
+//
+//
+//
+//    val sql = "INSERT INTO users (name, password, admin) values ('nns', 'hoge', false);"
+//    val result = statement.executeUpdate(sql)
+////    System.out.println("結果１：" + result)
+//    s"結果：$result\n"
+
+
+  }
 }
 
 class Bootstrap extends LifeCycle {
