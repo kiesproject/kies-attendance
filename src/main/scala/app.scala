2ec9f9b123275d7b807f21a6785177ec9561fea6
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2018-04-19 00:47:23 +0900

    [wip] connect cloud sql

--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -8,17 +8,18 @@ object Hello extends WebApp {
   // v本当はダメですがSQLのパスメモっときます
   // x8pzmgnILG337ylj
   get("/say-hello") {
-    s"Hello, ${params.getOrElse("message", "Anonymous")}!\n"
+//    s"Hello, ${params.getOrElse("message", "Anonymous")}!\n"
 
-//    val connection = DriverManager.getConnection("jdbc:google:mysql://myinstance/users", "root", "x8pzmgnILG337ylj")
-//    val statement = connection.createStatement
-//
-//
-//
-//    val sql = "INSERT INTO users (name, password, admin) values ('nns', 'hoge', false);"
+    val connection = DriverManager.getConnection("jdbc:google:mysql://myinstance/users?user=$root&password=x8pzmgnILG337ylj")
+    val statement = connection.createStatement
+
+
+
+    val sql = "INSERT INTO users (name, password, admin) values ('nns', 'hoge', false);"
+    val resultSet = statement.executeQuery("select * from users")
 //    val result = statement.executeUpdate(sql)
-////    System.out.println("結果１：" + result)
-//    s"結果：$result\n"
+//    System.out.println("結果１：" + result)
+    s"result $resultSet\n"
 
 
   }
